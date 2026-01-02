d4141d07712551913cc3f974e2d5130bc74e8ce3
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2026-01-02 09:11:45 +0800

    Installation of React-Router-Dom | Initial Setup of pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import RegisterPage from "./Pages/RegisterPage";
+
 export default function App() {
   return (
     <main>
-      <h1 className="text-9xl text-red-500">Hello World</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
     </main>
   );
 }
