3cf892a942ea009f50a81947c38b33158d6ec1d9
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2026-01-03 14:57:48 +0800

    Navbar | version 1.1.0

--- src/Components/UI/Navbar.jsx
+++ src/Components/UI/Navbar.jsx
@@ -1,10 +1,21 @@
 import { MessageCircleIcon, UserCircle2 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const goToHomePage = () => {
+    navigate("/");
+  };
   return (
     <main>
       <nav className="px-5 w-full h-16 items-center flex justify-between bg-sky-300 fixed ">
-        <h1 className=" text-xl font-semibold text-white ">UniTrade</h1>
+        <h1
+          className=" text-xl font-semibold text-white "
+          onClick={goToHomePage}
+        >
+          UniTrade
+        </h1>
         <ul className=" flex gap-5">
           <li>
             <a href="/Chats">
