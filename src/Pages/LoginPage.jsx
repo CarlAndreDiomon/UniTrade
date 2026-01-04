a1660b813436c53c87e31c36230fc2d446ff463f
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2026-01-04 23:48:29 +0800

    Login Page | version 2.0.0

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { ArrowLeft } from "lucide-react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
 
 export default function LoginPage() {
   const [seePassword, setSeePassword] = useState(false);
@@ -11,13 +12,29 @@ export default function LoginPage() {
     navigate("/");
   };
 
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { login, isLoggingIn } = useAuthStore();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    login(formData);
+  };
+
   return (
     <main className=" w-screen h-screen bg-blue-200 ">
       <button className="pt-2 pl-2" onClick={goToLandingPage}>
         <ArrowLeft />
       </button>
       <section className=" h-4/5 flex justify-center items-center">
-        <form action="" className=" gap-4 grid items-center w-screen px-8">
+        <form
+          action=""
+          onSubmit={handleSubmit}
+          className=" gap-4 grid items-center w-screen px-8"
+        >
           <h1 className="text-center text-4xl font-semibold">Login</h1>
           <input
             type="email"
@@ -25,6 +42,10 @@ export default function LoginPage() {
             id=""
             placeholder="Email"
             required
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
             className="hover:border-black border border-gray-600 rounded-lg pl-3 py-1"
           />
           <input
@@ -33,6 +54,10 @@ export default function LoginPage() {
             id=""
             placeholder="Password"
             required
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
             className="hover:border-black border border-gray-600 rounded-lg pl-3 py-1"
           />
           <button
@@ -46,6 +71,7 @@ export default function LoginPage() {
           </button>
           <button
             type="submit"
+            disabled={isLoggingIn}
             className="border p-1 text-xl mx-14 rounded-full"
           >
             Login
