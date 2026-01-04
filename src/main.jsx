a747afd37567b23676b8a9ff5ea08ef6f6857c3b
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2026-01-05 03:23:24 +0800

    fix some issue in deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/UniTrade">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
