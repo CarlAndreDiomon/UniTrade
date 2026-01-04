2f997d45c9d97d5b1e1f466cce6936d61ca7e106
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2026-01-04 23:47:14 +0800

    Add authentication and authorization for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,60 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import LoginPage from "./Pages/LoginPage";
 import RegisterPage from "./Pages/RegisterPage";
 import LandingPage from "./Pages/LandingPage";
 import ChatPage from "./Pages/ChatPage";
 import Navbar from "./Components/UI/Navbar";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+import { Loader } from "lucide-react";
 
 export default function App() {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
   return (
     <main>
-      <nav>
+      <nav className={` ${authUser ? "block" : "hidden"} `}>
         <Navbar />
       </nav>
-      <Routes>
-        <Route path="/landing" element={<LandingPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/chats" element={<ChatPage />} />
-      </Routes>
+      <section>
+        <Routes>
+          <Route
+            path="/"
+            element={!authUser ? <LandingPage /> : <Navigate to="/home" />}
+          />
+          <Route
+            path="/home"
+            element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/home" />}
+          />
+          <Route
+            path="/register"
+            element={!authUser ? <RegisterPage /> : <Navigate to="/home" />}
+          />
+          <Route
+            path="/chats"
+            element={authUser ? <ChatPage /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </section>
+      <Toaster />
     </main>
   );
 }
